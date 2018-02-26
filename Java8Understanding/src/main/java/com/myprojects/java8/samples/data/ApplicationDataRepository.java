301c22e00600afc92121416395c498ff7879a806
skoduri <shivaprasad.koduri@openet.com>
2018-02-26 09:29:17 +0000

    Adding Streams examples

--- Java8Understanding/src/main/java/com/myprojects/java8/samples/data/ApplicationDataRepository.java
+++ Java8Understanding/src/main/java/com/myprojects/java8/samples/data/ApplicationDataRepository.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.myprojects.java8.model.Dish;
+import com.myprojects.java8.model.Trader;
+import com.myprojects.java8.model.Transaction;
 
 public class ApplicationDataRepository {
 
@@ -18,16 +20,35 @@ public class ApplicationDataRepository {
 				new Dish("pork", false, 800, Dish.Type.MEAT),
 				new Dish("beef", false, 700, Dish.Type.MEAT),
 				new Dish("chicken", false, 400, Dish.Type.MEAT),
-				new Dish("french fries", false, 530, Dish.Type.OTHER),
-				new Dish("rice", false, 350, Dish.Type.OTHER),
-				new Dish("season fruit", false, 120, Dish.Type.OTHER),
-				new Dish("pizza", false, 550, Dish.Type.OTHER),
+				new Dish("french fries", true, 530, Dish.Type.OTHER),
+				new Dish("rice", true, 350, Dish.Type.OTHER),
+				new Dish("season fruit", true, 120, Dish.Type.OTHER),
+				new Dish("pizza", true, 550, Dish.Type.OTHER),
 				new Dish("prawns", false, 300, Dish.Type.FISH),
 				new Dish("salmon", false, 450, Dish.Type.FISH)
 				);
 		
 		return menu;
 		
+	}
+	
+	public static List<Transaction> getTransactions(){
+		
+		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader mario = new Trader("Mario","Milan");
+		Trader alan = new Trader("Alan","Cambridge");
+		Trader brian = new Trader("Brian","Cambridge");
+		
+		List<Transaction> transactions = Arrays.asList(
+				new Transaction(brian, 2011, 300),
+				new Transaction(raoul, 2012, 1000),
+				new Transaction(raoul, 2011, 400),
+				new Transaction(mario, 2012, 710),
+				new Transaction(mario, 2012, 700),
+				new Transaction(alan, 2012, 950)
+				);
+		
+		return transactions;
 	}
 
 }
--- Java8Understanding/src/main/java/com/myprojects/java8/samples/streams/StreamsDemo.java
+++ Java8Understanding/src/main/java/com/myprojects/java8/samples/streams/StreamsDemo.java
@@ -1,11 +1,16 @@
 package com.myprojects.java8.samples.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.myprojects.java8.model.Dish;
+import com.myprojects.java8.model.Trader;
+import com.myprojects.java8.model.Transaction;
 import com.myprojects.java8.samples.data.ApplicationDataRepository;
 
 public class StreamsDemo {
@@ -23,7 +28,12 @@ public class StreamsDemo {
 		calSquareOfNumber();
 		
 		problem1();
-
+		
+		findAndMatching(); //short-circuiting
+		
+		reducingOperations(); //a stream is reduced to a value
+		
+		traders_transaction_Operations();//traders and transactions examples
 	}
 	
 	private static void filterUniqueElements(){
@@ -89,11 +99,188 @@ public class StreamsDemo {
 	
 	
 	private static void problem1(){
+		//Unsolved
+		
 		//  Given two lists of numbers, how would you return all pairs of numbers? For example, given a
 		//	list [1, 2, 3] and a list [3, 4] you should return [(1, 3), (1, 4), (2, 3), (2, 4), (3, 3), (3, 4)]. 
+		List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+		List<Integer> numbers2 = Arrays.asList(3, 4);
+		
+//		List<int[]> pairs = numbers1.stream().flatMap(i -> numbers2.stream().map(j -> new int[]{i,j})).collect(Collectors.toList());
 		
 		
+		//Appending the question
+		//eturn only pairs whose sum is divisible by 3 //For example, (2, 4) and (3, 3) are valid.
+		
+//		List<int[]> pairs = numbers1.stream().flatMap(i -> numbers2.stream().filter(j -> (i + j) % 3 == 0).map(j -> new int[]{i,j})).collect(Collectors.toList());
 		
 	}
+	
+	private static void findAndMatching(){
+		
+		matchAtleastOneElement();
+		matchesAllElements();
+		matchesNone();
+		
+		findingAnElement();
+		findFirstElement();
+	}
+	
+	private static void matchAtleastOneElement(){
+		System.out.println("\nExecuting findAndMatching:: matchAtleastOneElement()..");
+		List<Dish> menu = ApplicationDataRepository.getDishes();
+		
+		if(menu.stream().anyMatch(Dish::isVegeterian)){
+			System.out.println("The menu is (somewhat) vegetarian friendly!!");
+			}
+	}
+	
+	
+	private static void matchesAllElements(){
+		System.out.println("\nExecuting findAndMatching:: matchesAllElements()..");
+		
+		boolean isHealthy = ApplicationDataRepository.getDishes().stream()
+				.allMatch(d -> d.getCalories() < 1000);
+		
+	}
+	
+	private static void matchesNone(){
+		System.out.println("\nExecuting findAndMatching:: matchesNone()..");
+		
+		boolean isHealthy = ApplicationDataRepository.getDishes().stream()
+				.noneMatch(d -> d.getCalories() >= 1000);
+	}
+	
+	private static void findingAnElement(){
+		System.out.println("\nExecuting findAndMatching:: findingAnElement()..");
+		
+		Optional<Dish> dish = ApplicationDataRepository.getDishes().stream().filter(Dish::isVegeterian).findAny();
+		
+		System.out.println(dish.isPresent() ? dish.get().getName() : null);
+		
+	}
+	
+	private static void findFirstElement(){
+		System.out.println("\nExecuting findAndMatching:: findFirstElement()..");
+		
+		List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+		Optional<Integer> firstSquareDivisibleByThree = someNumbers.stream().map(x -> x * x).filter(x -> x % 3 == 0).findFirst(); // 9
+		
+	}
+	
+	private static void reducingOperations(){
+		//In functional programming-language jargon, this is referred to as a fold
+		
+		summingElements();
+		multiplyingElements();
+		
+		maxElement();
+		minElement();
+	}
+	
+	
+	private static void summingElements(){
+		System.out.println("\nExecuting reducingOperations:: summingElements()..");
+		
+		int sum = Arrays.asList(1,2,3,4).stream().reduce(0, (a, b) -> a + b);  // sum
+		
+		int sumUsingLambda = Arrays.asList(1,2,3,4).stream().reduce(0, Integer::sum);  // Using lambda
+		
+		Optional<Integer> sumWithNoInitialValue = Arrays.asList(1,2,3,4).stream().reduce((a, b) -> (a + b));  //No initial value
+	}
+	
+	private static void multiplyingElements(){
+		System.out.println("\nExecuting reducingOperations:: multiplyingElements()..");
+
+		int product = Arrays.asList(1,2,3,4).stream().reduce(1, (a, b) -> a * b);   //multiply
+		
+	}
+	
+	
+	private static void maxElement(){
+		System.out.println("\nExecuting reducingOperations:: maxElement()..");
+		Optional<Integer> max = Arrays.asList(1,2,3,4).stream().reduce(Integer::max);
+	}
+	
+	private static void minElement(){
+		System.out.println("\nExecuting reducingOperations:: minElement()..");
+		Optional<Integer> min = Arrays.asList(1,2,3,4).stream().reduce(Integer::min);
+	}
+	
+	
+	private static void countNumOfDishes(){
+		System.out.println("\nExecuting reducingOperations:: countNumOfDishes()..");
+		int countNumOfDishes = ApplicationDataRepository.getDishes().stream().map(d -> 1).reduce(0, (a, b) -> a + b); //Count number od dishes
+
+	}
+	
+	
+	private static void traders_transaction_Operations(){
+		
+//		1. Find all transactions in the year 2011 and sort them by value (small to high).
+		
+		List<Transaction> tr2011 = ApplicationDataRepository.getTransactions()
+				.stream()
+				.filter(transaction -> transaction.getYear() == 2011)
+				.sorted(Comparator.comparing(Transaction::getValue))
+				.collect(Collectors.toList());
+		
+//		2. What are all the unique cities where the traders work?
+		
+		List<String> citiesList = ApplicationDataRepository.getTransactions()
+				.stream()
+				.map(transaction -> transaction.getTrader().getCity())
+				.distinct()
+				.collect(Collectors.toList());
+		
+		Set<String> citiesSet = ApplicationDataRepository.getTransactions()
+				.stream()
+				.map(transaction -> transaction.getTrader().getCity())
+				.collect(Collectors.toSet());
+		
+//		3. Find all traders from Cambridge and sort them by name.
+		
+		List<Trader> traders = ApplicationDataRepository.getTransactions()
+				.stream()
+				.map(Transaction::getTrader)
+				.filter(trader -> trader.getCity().equals("Cambridge"))
+				.distinct().sorted(Comparator.comparing(Trader::getName))
+				.collect(Collectors.toList());
+		
+//		4. Return a string of all traders’ names sorted alphabetically.
+		
+		String traderStr_using_reduce = ApplicationDataRepository.getTransactions()
+				.stream()
+				.map(transaction -> transaction.getTrader().getName())
+				.distinct()
+				.sorted()
+				.reduce("",(n1,n2) -> n1+ n2);
+		
+		String traderStr_using_joining = ApplicationDataRepository.getTransactions()
+				.stream()
+				.map(transaction -> transaction.getTrader().getName())
+				.distinct()
+				.sorted()
+				.collect(Collectors.joining());
+				
+		
+//		5. Are any traders based in Milan?
+		
+		boolean milanBased = ApplicationDataRepository.getTransactions().stream().anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan")); 
+		
+//		6. Print all transactions’ values from the traders living in Cambridge.
+		
+		ApplicationDataRepository.getTransactions().stream().filter(t -> "Cambridge".equals(t.getTrader().getCity())).map(Transaction::getValue).forEach(System.out::println);
+		
+//		7. What’s the highest value of all the transactions?
+		
+		Optional<Integer> highestValue = ApplicationDataRepository.getTransactions().stream().map(Transaction::getValue).reduce(Integer::max);
+		
+//		8. Find the transaction with the smallest value.
+		
+		Optional<Transaction> smallestTransactions = ApplicationDataRepository.getTransactions().stream().reduce( (t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2);
+		
+	}
+	
 
 }
