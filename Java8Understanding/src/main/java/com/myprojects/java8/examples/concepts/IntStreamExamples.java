cfab26d6e48aa392e9035d327cecde6e849a2a2c
skoduri <shivaprasad.koduri@openet.com>
2018-03-12 07:55:12 +0000

    Adding examples

--- Java8Understanding/src/main/java/com/myprojects/java8/examples/concepts/IntStreamExamples.java
+++ Java8Understanding/src/main/java/com/myprojects/java8/examples/concepts/IntStreamExamples.java
@@ -7,6 +7,15 @@ public class IntStreamExamples {
 	public static void main(String[] args) {
 		//IntStream allMatch(IntPredicate predicate)
 		allMatchWithIntStream();
+		
+		//IntStream anyMatch(IntPredicate predicate) returns whether any elements of this stream match the provided predicate.
+		anyMatchWithIntStream();
+	}
+
+	private static void anyMatchWithIntStream() {
+		IntStream i = IntStream.of(6,5,7,1, 2, 3, 3);
+	    boolean d = i.anyMatch(n-> n > 0 );
+	    System.out.println(d);
 	}
 
 	private static void allMatchWithIntStream() {
