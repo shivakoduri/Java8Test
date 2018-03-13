11e58045757a9457014258a1b10c9e8a86a6ee05
skoduri <shivaprasad.koduri@openet.com>
2018-03-13 09:31:38 +0000

    adding examples for concatenate streams

---
+++ Java8Understanding/src/main/java/com/myproject/java8/forums/examples/forum1/ConcatenateStreamsDemo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.myproject.java8.forums.examples.forum1;
+
+/**
+ * @author skoduri
+ *
+ */
+public class ConcatenateStreamsDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Concat Streams
+		
+		
+		//Concat Lists
+		
+		
+		//Concat Arrays
+		
+		//Concat Sets
+
+	}
+
+}
